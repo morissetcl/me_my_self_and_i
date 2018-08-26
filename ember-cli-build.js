eaec1c6d545fb4f442ec8a083c1c8e00136d1666
Clement Morisset <morissetcl87@gmail.com>
2018-08-26 17:17:36 +0200

    add dotenv

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    dotEnv: {
+      clientAllowedKeys: ['CLOUD_NAME', 'CLOUDINARY_API_KEY'],
+      path: {
+        development: '/.env'
+      }
+    }
   });
-
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
