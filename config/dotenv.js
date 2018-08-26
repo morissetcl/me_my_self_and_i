5f5f82ceb87dc898dcde05ca160767cf8699c7b7
Clement Morisset <morissetcl87@gmail.com>
2018-08-26 17:47:01 +0200

    fix deploy

--- config/dotenv.js
+++ config/dotenv.js
@@ -1,8 +1,28 @@
+
+/* eslint-env node */
+
+'use strict';
+
+const path = require('path');
+
 module.exports = function(env) {
+  // $ ember deploy production
+  // [0] = node
+  // [1] = ember
+  // [2] command = deploy
+  // [3] target = production
+  const [,, command, target] = process.argv;
+
+  if (command && target && command === 'deploy') {
+    env = target;
+  }
+
+  const isProd = env == 'production';
+  const isStaging = env == 'staging';
+  const envFile = (isProd || isStaging) ? `.env.deploy.${env}` : '.env';
+
   return {
     clientAllowedKeys: ['CLOUD_NAME', 'CLOUDINARY_API_KEY'],
-    // Fail build when there is missing any of clientAllowedKeys environment variables.
-    // By default false.
-    failOnMissingKey: false,
+    path: path.join(__dirname, '..', envFile)
   };
 };
