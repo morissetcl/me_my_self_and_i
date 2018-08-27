15c0587cfb88addce19e81eaa91c150f40a85e4c
Clement Morisset <morissetcl87@gmail.com>
2018-08-27 12:17:58 +0200

    order article by date

--- app/routes/articles.js
+++ app/routes/articles.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.get('store').findAll('article');
+    return this.get('store').findAll('article').then(results => results.sortBy('createdAt'));
   }
  });
