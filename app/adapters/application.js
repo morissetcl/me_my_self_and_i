a42a134af85d53a85fc46d8e8cb4add474897979
Clement Morisset <morissetcl87@gmail.com>
2018-09-01 14:38:45 +0200

    add article filter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -16,5 +16,4 @@ import DS from 'ember-data';
 export default DS.JSONAPIAdapter.extend({
   host: 'https://mmandi-api.herokuapp.com'
   // host: 'http://localhost:3000'
-
 });
--- app/routes/articles.js
+++ app/routes/articles.js
@@ -2,7 +2,15 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.get('store').findAll('article')
-                            .then(results => results.sortBy('createdAt').reverse());
-  }
+    return this.get('store', function(db, request) {
+        if(request.queryParams.tags !== undefined) {
+          let filteredArticles = articles.filter(function(i) {
+            return i.attributes.tags.toLowerCase().indexOf(request.queryParams.tags.toLowerCase()) !== -1;
+          });
+          return { data: filteredArticles };
+        } else {
+          return { data: articles };
+        }
+      });
+    }
  });
