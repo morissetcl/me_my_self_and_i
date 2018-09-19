c618ba819ac2c61c2c28a544426a27c2e643c99a
Clement Morisset <morissetcl87@gmail.com>
2018-09-19 08:47:12 +0200

    aoute node-sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+let nodeSass = require('node-sass');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     dotEnv: {
       clientAllowedKeys: ['CLOUD_NAME', 'CLOUDINARY_API_KEY']
+    },
+    sassOptions: {
+      implementation: nodeSass
     }
   });
 
